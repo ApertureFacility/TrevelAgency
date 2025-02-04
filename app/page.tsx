488d9e477133db833676db07b415b95afcf51f60
Sergey FrontendDeveloper <sergeyt.devwizard@gmail.com>
2025-02-04 15:42:33 +0300

    doc fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,16 @@
 "use client";
 
-import { BrowserRouter } from "react-router-dom";
 
 import MainPage from "./components/MainPage/MainPage";
 
 export default function Home() {
   return (
-    <BrowserRouter>
+
       <div className="page">
         <main className="main">
           <MainPage />
         </main>
       </div>
-    </BrowserRouter>
+
   );
 }
